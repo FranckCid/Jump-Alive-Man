3a370027a016cde5cf380f237124fa325b6cd036
Franck Cid <juniorfchj2@gmail.com>
2017-07-21 18:34:59 -0300

    Overload Vector2 Operators

--- gameobject.cpp
+++ gameobject.cpp
@@ -10,15 +10,17 @@ void GameObject::update(Input& input, float elapsedTime){
 	float spd = 3.0f * elapsedTime;
 	std::cout << elapsedTime << std::endl;
 	if(input.isKeyHold(SDL_SCANCODE_LEFT)){
-		_pos.x = _pos.x - spd;
+		_pos.x -= spd;
 	}else if(input.isKeyHold(SDL_SCANCODE_RIGHT)){
-		_pos.x = _pos.x + spd;
+		_pos.x += spd;
 	}
 	if(input.isKeyHold(SDL_SCANCODE_UP)){
-		_pos.y = _pos.y - spd;
+		_pos.y -= spd;
 	}else if(input.isKeyHold(SDL_SCANCODE_DOWN)){
-		_pos.y = _pos.y + spd;
+		_pos.y += spd;
 	}
+	Vector2f a(0, 0);
+	_pos = a + _pos;
 }
 
 void GameObject::draw(Graphics&g){
--- vector2.h
+++ vector2.h
@@ -5,6 +5,23 @@ template <typename T>
 class __Vector2{
 public:
 	__Vector2(T a, T b) : x(a), y(b){}
+	__Vector2(const __Vector2&v) : __Vector2(v.x, v.y){}
+	__Vector2& operator+=(const __Vector2&v){
+		x += v.x;
+		y += v.y;
+		return *this;
+	}
+	__Vector2& operator-=(const __Vector2&v){
+		x -= v.x;
+		y -= v.y;
+		return *this;
+	}
+	__Vector2& operator+(const __Vector2&v) const{
+		return __Vector2(*this) += v;
+	}
+	__Vector2& operator-(const __Vector2&v) const{
+		return __Vector2(*this) += v;
+	}
 	T x;
 	T y;
 };
